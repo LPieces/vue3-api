5d70c404e9797448d292ebde0b601d984b956aa3
LPieces <ljh_pieces@163.com>
2021-07-20 18:02:27 +0800

    复习生命周期函数

---
+++ src/components/LifeCycleHooks.vue
@@ -0,0 +1,26 @@
+<template>
+    
+</template>
+
+<script>
+import { getCurrentInstance, onBeforeMount, onMounted, onUpdated, watchEffect } from 'vue'
+export default {
+    name: 'LifeCycleHooks',
+    setup(porps, ctx){
+        const instance = getCurrentInstance();
+        console.log(instance); // 获取当前组件的实例
+        onMounted(()=>{
+            console.log('onMounted')
+        })
+        onBeforeMount(()=>{
+            console.log('onBeforeMount')
+        })
+        onUpdated(()=>{
+            console.log("onUpdated");
+        })
+        watchEffect(()=>{
+            console.log('watchEffect');
+        })
+    }
+}
+</script>
